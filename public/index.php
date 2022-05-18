ac68d7fe38605031111d46af28babe8c8ebf050b
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-05-18 20:35:54 +0630

    Using PHP-DI

--- public/index.php
+++ public/index.php
@@ -2,9 +2,22 @@
 
 declare(strict_types=1);
 
+use DI\ContainerBuilder;
 use ExampleApp\HelloWorld;
+use function DI\create;
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-$hello = new HelloWorld();
-$hello->announce();
+
+$containerBuilder = new ContainerBuilder();
+
+$containerBuilder->useAutowiring(false);
+$containerBuilder->useAnnotations(false);
+$containerBuilder->addDefinitions([
+    HelloWorld::class => create(HelloWorld::class),
+]);
+
+$container = $containerBuilder->build();
+
+$helloWorld = $container->get(HelloWorld::class);
+$helloWorld->announce();
